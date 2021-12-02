00d2f0a65e4d69701fee67f2c2fe7b3d4b383116
David Hong <phazex1@gmail.com>
2021-12-02 18:32:52 -0500

    Adding Module3

--- src/app.js
+++ src/app.js
@@ -1,6 +1,7 @@
 const fs = require('fs')
 const path = require('path')
 const express = require('express')
+const { REFUSED } = require('dns')
 const app = express()
 const accountData = fs.readFileSync('src/json/accounts.json', 'utf8')
 const accounts = JSON.parse(accountData)
@@ -11,25 +12,63 @@ app.set('view engine', 'ejs')
 app.set('views', path.join(__dirname, 'views'))
 
 app.use(express.static(path.join(__dirname, '/public')))
+app.use(express.urlencoded({ extended: true }))
 
-app.get('/', function(req, res) {
+app.get('/', (req, res) => {
     res.render('index', { title: 'Account Summary', accounts: accounts })
 })
 
-app.get('/savings', function (req, res) {
+app.get('/savings', (req, res) => {
     res.render('account', { account: accounts.savings })
 })
 
-app.get('/checking', function (req, res) {
+app.get('/checking', (req, res) => {
     res.render('account', { account: accounts.checking })
 })
 
-app.get('/credit', function (req, res) {
+app.get('/credit', (req, res) => {
     res.render('account', { account: accounts.credit })
 })
 
-app.get('/profile', function (req, res) {
+app.get('/profile', (req, res) => {
     res.render('profile', { user: users[0] })
+})
+
+app.get('/transfer', (req, res) => {
+    res.render('transfer')
+})
+
+app.get('/payment', (req, res) => {
+    res.render('payment', { account: accounts.credit })
+})
+
+app.post('/transfer', (req, res) => {
+    const fromAmount = accounts['savings'].balance - req.body.amount
+    accounts['savings'].balance = fromAmount
+
+    const checkingAcct = parseInt(accounts['checking'].balance)
+    const toAmount = checkingAcct + parseInt(req.body.amount)
+    accounts['checking'].balance = toAmount
+
+    const accountsJSON = JSON.stringify(accounts)
+    
+    fs.writeFileSync(path.join(__dirname, 'json', 'accounts.json'), accountsJSON, 'utf8')
+    
+    res.render('transfer', { message: "Transfer Completed" })
+})
+
+app.post('/payment', (req, res) => {
+    accounts.credit.balance -= req.body.amount
+    accounts.credit.available += parseInt(req.body.amount)
+
+    const accountsJSON = JSON.stringify(accounts)
+
+    fs.writeFileSync(path.join(__dirname, 'json', 'accounts.json'), accountsJSON, 'utf8')
+
+    res.render('payment', { message: "Payment Successful", account: accounts.credit } )
+
+
+
 })
 
 app.listen(3000, () => {
